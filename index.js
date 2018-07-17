ad7aca44c8d2cdcc952a4d5fe360afd70cc97850
joanneelizabethhooton@gmail.com <joanneelizabethhooton@gmail.com>
2018-07-17 14:09:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,30 +21,10 @@ function currentLine(line) {
   var lineNamesandNumbers = [];
 
     for(var i = 0; i < line.length; i++) {
-      lineNamesandNumbers.push(i+". "+ name)
+      lineNamesandNumbers.push((i+1)+". "+ name)
     }
 
 
     return "The line is currently: " + lineNamesandNumbers.join(', ');
 
 }
-
-var katzDeliLine = [];
-
-takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
-takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
-takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
-
-nowServing(katzDeliLine); // "Currently serving Ada."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent"
-
-takeANumber(katzDeliLine, "Matz"); // "3"
-
-currentLine(katzDeliLine); // "The line is currently: 1. Grace, 2. Kent, 3. Matz"
-
-nowServing(katzDeliLine); // "Currently serving Grace."
-
-currentLine(katzDeliLine); // "The line is currently: 1. Kent, 2. Matz"
