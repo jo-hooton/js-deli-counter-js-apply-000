0b91e0541d024d20a6b881e477013710d235ee30
joanneelizabethhooton@gmail.com <joanneelizabethhooton@gmail.com>
2018-07-17 13:52:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var katzDeli = [];
 
 function takeANumber(line, name) {
   line.push(name);
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
 
   return "Welcome, " + name + ". You are number " + line.length + " in line."
 }
